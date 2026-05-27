62cc1ca27aec434ed7431ece1eafac041a77b1f3
RhynoBotha <rhynobotha39@gmail.com>
2026-05-27 09:57:42 +0200

    Section B: Question 1.1

---
+++ SectionB/SectionB-Code-RhynoBotha/SectionB-Code-RhynoBotha/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Welcome to the Structure of a C# Program Demo!");
+Console.Write("Enter your name:");
+string name = Console.ReadLine();
+Console.WriteLine($"Hello {name}\n");
+
+Console.WriteLine("Program Structure Demonstrated:");
+Console.WriteLine("1. using System : imports functionality");
+Console.WriteLine("2. namespace : organizes code");
+Console.WriteLine("3. class Program : container for code");
+Console.WriteLine("4. Main() : entry point of program");
+Console.WriteLine("5. Comments : explain logic and documentation");
+Console.WriteLine("Program executed successfully");
+Console.ReadKey();
